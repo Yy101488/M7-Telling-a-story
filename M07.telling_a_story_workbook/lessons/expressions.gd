1f51eb0f21a16d1cada91476fe898faab04ee936
Yy101488 <yili25@s.sfusd.edu>
2025-10-31 11:15:49 -0700

    Finished Module 7 Lesson 7

---
+++ M07.telling_a_story_workbook/lessons/expressions.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
+@onready var button_sophia: Button = %ButtonSophia
+@onready var button_pink: Button = %ButtonPink
+@onready var button_rugular: Button = %ButtonRugular
+@onready var button_sad: Button = %ButtonSad
+@onready var button_happy: Button = %ButtonHappy
